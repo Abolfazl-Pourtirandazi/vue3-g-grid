a96673844b3399870a2022354bde31ba3c7fc837
overhack_dev <death_angel_you@yahoo.com>
2024-06-21 14:45:22 +0330

    add: gicon component

---
+++ src/components/icon/GIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <i :class="`g-icon ${icon}`" :style="{ transform: `rotate(${rotation}deg)` }" v-bind="$attrs"></i>
+</template>
+
+<script lang="ts" setup>
+import type { IconProps } from "@/@types/icon";
+
+defineOptions({ name: "GIcon", inheritAttrs: false });
+
+withDefaults(defineProps<IconProps>(), {
+  rotation: 0
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./gIcon.scss";
+</style>
