f9bee48be34ef9bc5fe3fd6a4abaf244b7fd65f8
overhack_dev <death_angel_you@yahoo.com>
2024-06-23 13:59:46 +0330

    add: spinner component

---
+++ lib/components/spinner/GSpinner.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="g-spinner" role="status"></div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({ name: "GSpinner" });
+</script>
+
+<style lang="scss">
+@import "./gSpinner.scss";
+</style>
