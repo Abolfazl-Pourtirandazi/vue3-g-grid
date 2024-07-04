a2dbad532510c0d997cfbf754254709e5e7356e8
overhack_dev <death_angel_you@yahoo.com>
2024-07-04 17:24:41 +0330

    remove: import style.scss in plugin

--- lib/plugin/index.ts
+++ lib/plugin/index.ts
@@ -1,6 +1,5 @@
 import type { App } from "vue";
 import GGrid from "../components/grid/GGrid.vue";
-import "../assets/scss/style.scss";
 
 export interface GGridOptions {
   componentName: string | undefined;
