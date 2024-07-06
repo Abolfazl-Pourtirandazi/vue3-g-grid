293af0b8b5a9a9f74d1db4e45b9cf75e4c514f42
overhack_dev <death_angel_you@yahoo.com>
2024-07-06 15:49:16 +0330

    update: export types

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,28 @@
 import { GGridPlugin, type GGridOptions } from "./plugin";
 import GGrid from "./components/grid/GGrid.vue";
+import type {
+  GridProps,
+  GridColumn,
+  GridType,
+  GridAggregates,
+  GridAggregateType,
+  GridSort,
+  SortOptions,
+  SortType
+} from "./types/grid";
 
-export { GGrid, type GGridOptions };
+export type {
+  GGridOptions,
+  GridProps,
+  GridColumn,
+  GridType,
+  GridAggregates,
+  GridAggregateType,
+  GridSort,
+  SortOptions,
+  SortType
+};
+
+export { GGrid };
 
 export default GGridPlugin;
