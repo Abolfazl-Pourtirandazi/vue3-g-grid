21ac98be1305464ca89465b4dddf4e65e6723517
overhack_dev <death_angel_you@yahoo.com>
2024-07-08 16:23:42 +0330

    update: support publishing to npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
-import { fileURLToPath, URL } from "node:url";
-
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
+import del from "rollup-plugin-delete";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+  build: {
+    outDir: "./dist",
+    cssCodeSplit: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "g-grid",
+      fileName: "g-grid"
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: { vue: "Vue" }
+      },
+      plugins: [del({ targets: ["dist/favicon.svg", "dist/favicon.ico"], hook: "generateBundle" })]
     }
   }
 });
